aebe7e376aebfab41ee0757d7dd5b14ca75d79ba
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-05-23 16:09:31 -0300

    stupid coaching rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,17 +5,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @answer = coach(@question)
-  end
-
-  def coach(question)
-    if question.downcase == "i am going to work!"
-      "Great!"
-    elsif question.end_with?("?")
-      "Silly question, get dressed and go to work!"
+    if @question.downcase == "i am going to work!"
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
-
 end
